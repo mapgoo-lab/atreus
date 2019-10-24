84eff6a4bb53d4adbe1a6db85c6d34cd6cd15e55
zhuyx <zhuyx@mapgoo.net>
2019-10-24 10:14:01 +0800

    修复一个编译问题

--- pkg/naming/etcd/etcd.go
+++ pkg/naming/etcd/etcd.go
@@ -15,7 +15,7 @@ import (
 	"github.com/mapgoo-lab/atreus/pkg/log"
 	"github.com/mapgoo-lab/atreus/pkg/naming"
 	"go.etcd.io/etcd/clientv3"
-	"go.etcd.io/etcd/mvcc/mvccpb"
+	"github.com/coreos/etcd/mvcc/mvccpb"
 	"google.golang.org/grpc"
 )
 
