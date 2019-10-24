6ee357ea35c3d81665851d153fb543c10e803ae7
zhuyx <1154@mapgoo.net>
2019-10-24 14:42:35 +0800

    同步master分支的代码

---
+++ pkg/queue/databus/consumer.go
@@ -0,0 +1,128 @@
+package databus
+
+import (
+	"fmt"
+	"time"
+	"errors"
+	"context"
+	"github.com/Shopify/sarama"
+)
+
+//使用者必须实现的接口
+type ConsumerDeal interface {
+	//数据处理的实现
+	DealMessage(data []byte) error
+
+	//消费组增加消费者的消息通知
+	Setup(topicAndPartitions map[string][]int32, memberId string, generationId int32)
+
+	//消费组中消费者退出的消息通知
+	Cleanup(topicAndPartitions map[string][]int32, memberId string, generationId int32)
+}
+
+type ConsumerEvent interface {
+	//启动轮询消费数据
+	Start() error
+
+	//关闭消费者，必须调用
+	Close()
+}
+
+type consumerEvent struct {
+	address []string
+	groupid string
+	topic string
+	consumer sarama.ConsumerGroup
+	deal ConsumerDeal
+}
+
+type ConsumerParam struct {
+	Address []string
+	GroupId string
+	Topic string
+	DealHanle ConsumerDeal
+	KafkaVer string
+}
+
+func NewConsumer(param ConsumerParam) (ConsumerEvent, error) {
+	config := sarama.NewConfig()
+
+	//是否接收返回的错误消息,当发生错误时会放到Error这个通道中.从它里面获取错误消息
+	config.Consumer.Return.Errors = true
+	
+	//指定Offset,也就是从哪里获取消息,默认时从主题的开始获取
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	
+	//失败后再次尝试的间隔时间
+	config.Consumer.Retry.Backoff = 2 * time.Second
+
+	//最大等待时间
+	config.Consumer.MaxWaitTime = 250 * time.Millisecond
+	config.Consumer.MaxProcessingTime = 100 * time.Millisecond
+
+	// 提交跟新Offset的频率
+	config.Consumer.Offsets.CommitInterval = 1 * time.Second
+
+	//消费组内的消费者消费的算法（BalanceStrategySticky、BalanceStrategyRange、BalanceStrategyRoundRobin）
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin;
+
+    //设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
+    //注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
+    version, err := sarama.ParseKafkaVersion(param.KafkaVer)
+	if err != nil {
+		fmt.Println("Error parsing Kafka version: %v", err)
+		return nil, err
+	}
+	config.Version = version
+
+    consumer, err := sarama.NewConsumerGroup(param.Address, param.GroupId, config)
+    if err != nil {
+        fmt.Println(err)
+        return nil, err
+	}
+
+	return &consumerEvent{
+		address: param.Address,
+		groupid: param.GroupId,
+		topic: param.Topic,
+		consumer: consumer,
+		deal: param.DealHanle,
+	}, nil
+}
+
+func (handle *consumerEvent) Start() error {
+	ctx := context.Background()
+	topics := []string{handle.topic};
+	for {
+		handler := consumerGroupHandler{handle}
+		if err := handle.consumer.Consume(ctx, topics, handler); err != nil {
+			fmt.Printf("Error from consumer: %v", err)
+			return errors.New("Error from consumer.")
+		}
+	}
+}
+
+func (handle *consumerEvent) Close() {
+	handle.consumer.Close()
+}
+
+type consumerGroupHandler struct {
+	event *consumerEvent
+}
+
+func (handle consumerGroupHandler) Setup(sess sarama.ConsumerGroupSession) error {
+	handle.event.deal.Setup(sess.Claims(), sess.MemberID(), sess.GenerationID())
+	return nil 
+}
+func (handle consumerGroupHandler) Cleanup(sess sarama.ConsumerGroupSession) error {
+	handle.event.deal.Cleanup(sess.Claims(), sess.MemberID(), sess.GenerationID())
+	return nil 
+}
+func (handle consumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for msg := range claim.Messages() {
+		fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+		handle.event.deal.DealMessage(msg.Value)
+		sess.MarkMessage(msg, "")
+	}
+	return nil
+}
---
+++ pkg/queue/databus/producer.go
@@ -0,0 +1,110 @@
+package databus
+
+import (
+	"fmt"
+	"time"
+	"errors"
+	"github.com/Shopify/sarama"
+)
+
+type ProducerEvent interface {
+	//发送消息接口
+	SendMessage(data []byte, key string) error
+
+	//关闭生产者
+	Close()
+}
+
+type producerEvent struct {
+	address []string
+	topic string
+	isack bool
+	producer sarama.AsyncProducer
+}
+
+type ProducerParam struct {
+	Address []string
+	Topic string
+	IsAck bool
+	KafkaVer string
+}
+
+func NewAsyncProducer(param ProducerParam) (ProducerEvent, error) {
+	config := sarama.NewConfig()
+	config.Net.MaxOpenRequests = 10
+	config.Net.DialTimeout = 30 * time.Second
+	config.Net.ReadTimeout = 30 * time.Second
+	config.Net.WriteTimeout = 30 * time.Second
+
+    //等待服务器所有副本都保存成功后的响应
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	
+    //通过msg中的key生成hash值,选择分区
+	config.Producer.Partitioner = sarama.NewHashPartitioner
+	
+    //是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
+    if param.IsAck == true {
+		config.Producer.Return.Successes = true
+		config.Producer.Return.Errors = true
+	}
+
+	config.Producer.Timeout = 10 * time.Second
+
+	//重试次数
+	config.Producer.Retry.Max = 3 
+	config.Producer.Retry.Backoff = 100 * time.Millisecond
+	
+    //设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
+    //注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
+    version, err := sarama.ParseKafkaVersion(param.KafkaVer)
+	if err != nil {
+		fmt.Println("Error parsing Kafka version: %v", err)
+		return nil, err
+	}
+	config.Version = version
+
+    producer, err := sarama.NewAsyncProducer(param.Address, config)
+    if err != nil {
+        fmt.Println(err)
+        return nil, err
+	}
+
+	return &producerEvent{
+		address: param.Address,
+		topic: param.Topic,
+		isack: param.IsAck,
+		producer: producer,
+	}, nil
+}
+
+func (handle *producerEvent) SendMessage(data []byte, key string) error {
+	// 注意：这里的msg必须得是新构建的变量，不然你会发现发送过去的消息内容都是一样的，因为批次发送消息的关系。
+	msg := &sarama.ProducerMessage{
+		Topic: handle.topic,
+		Key:sarama.ByteEncoder(key),
+		Value:sarama.ByteEncoder(data),
+	}
+
+	//使用通道发送
+	handle.producer.Input() <- msg
+
+	if handle.isack == true {
+		select {
+		case suc := <-handle.producer.Successes():
+			fmt.Println("offset: ", suc.Offset, "timestamp: ", suc.Timestamp.String(), "partitions: ", suc.Partition)
+			return nil
+		case fail := <-handle.producer.Errors():
+			fmt.Println("err: ", fail.Err)
+			return fail.Err
+		case timeout := <-time.After(time.Second*10): 
+			fmt.Println("ack msg error %p.", timeout)
+			return errors.New("ack msg timeout.")
+		}
+	}
+
+	return nil
+}
+
+func (handle *producerEvent) Close() {
+	handle.producer.AsyncClose()
+}
